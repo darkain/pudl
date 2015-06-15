4fee251f96dc8a71665ddf92216cca8e958442e4
Darkain (hagens) <git@darkain.com>
2015-06-15 12:17:44 -0700

    added pudlFunction::timestamp()

--- pudlFunction.php
+++ pudlFunction.php
@@ -2,13 +2,19 @@
 
 class pudlFunction {
 
-	static function __callStatic($name, $arguments) {
+	public static function __callStatic($name, $arguments) {
 		$value = new pudlFunction();
 
 		$name = '_' . strtoupper($name);
 		$value->$name = $arguments;
 
 		return $value;
+	}
+
+
+	public static function timestamp() {
+		global $db;
+		return self::from_unixtime($db->time());
 	}
 
 }
