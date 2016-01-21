2362eba275275ced5240afd41fa58c1c6ff55167
Vince <git@darkain.com>
2016-01-21 10:25:48 -0800

    Fixing PHP compatibility

--- pudlAlias.php
+++ pudlAlias.php
@@ -4,7 +4,7 @@
 trait pudlAlias {
 
 	protected function _alias() {
-		return $this->_table('x_pudl_alias_' . ++$this->_aliases);
+		return $this->_table('x_pudl_alias_' . ++self::$_aliases);
 	}
 
 	private static $_aliases = 0;
