de957ee071f32da08141c1b0222da94a12ac4840
Vince <git@darkain.com>
2016-04-12 10:49:24 -0700

    Moved ::json function over to dynamic trait, and cleaned up trait

--- pudlDynamic.php
+++ pudlDynamic.php
@@ -4,82 +4,87 @@
 trait pudlDynamic {
 
 
-	static function dynamic_binary($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('BINARY'), $length)
+	public static function json($column) {
+		return self::column_json( self::column($column) );
+	}
+
+
+	public static function dynamic_binary($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('BINARY'), $length)
 		);
 	}
 
 
-	static function dynamic_char($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('CHAR'), $length)
+	public static function dynamic_char($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('CHAR'), $length)
 		);
 	}
 
 
-	static function dynamic_date($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('DATE'), $length)
+	public static function dynamic_date($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('DATE'), $length)
 		);
 	}
 
 
-	static function dynamic_datetime($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('DATETIME'), $length)
+	public static function dynamic_datetime($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('DATETIME'), $length)
 		);
 	}
 
 
-	static function dynamic_decimal($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('DECIMAL'), $length)
+	public static function dynamic_decimal($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('DECIMAL'), $length)
 		);
 	}
 
 
-	static function dynamic_double($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('DOUBLE'), $length)
+	public static function dynamic_double($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('DOUBLE'), $length)
 		);
 	}
 
 
-	static function dynamic_integer($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('INTEGER'), $length)
+	public static function dynamic_integer($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('INTEGER'), $length)
 		);
 	}
 
 
-	static function dynamic_signed($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('SIGNED'), $length)
+	public static function dynamic_signed($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('SIGNED'), $length)
 		);
 	}
 
 
-	static function dynamic_time($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('TIME'), $length)
+	public static function dynamic_time($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('TIME'), $length)
 		);
 	}
 
 
-	static function dynamic_unsigned($blob, $column, $length=false) {
-		return pudl::column_get(
-			pudl::column($blob),
-			new pudlAs($column, pudl::raw('UNSIGNED'), $length)
+	public static function dynamic_unsigned($blob, $column, $length=false) {
+		return self::column_get(
+			self::column($blob),
+			new pudlAs($column, self::raw('UNSIGNED'), $length)
 		);
 	}
 
