0402a49671a7eaaffd09d33954c3ce241a678949
Vince <git@darkain.com>
2016-04-12 10:48:33 -0700

    Fixed quote type in unit test

--- test/dynamic.php
+++ test/dynamic.php
@@ -2,49 +2,49 @@
 
 
 $db->string()->select(pudl::dynamic_binary('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS BINARY) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS BINARY) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_char('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS CHAR) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS CHAR) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_date('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS DATE) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS DATE) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_datetime('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS DATETIME) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS DATETIME) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_decimal('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS DECIMAL) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS DECIMAL) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_double('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS DOUBLE) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS DOUBLE) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_integer('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS INTEGER) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS INTEGER) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_signed('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS SIGNED) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS SIGNED) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_time('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS TIME) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS TIME) FROM `table`");
 
 
 $db->string()->select(pudl::dynamic_unsigned('table.column', 'dynamic'), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS UNSIGNED) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS UNSIGNED) FROM `table`");
 
 
 
 
 $db->string()->select(pudl::dynamic_binary('table.column', 'dynamic', 10), 'table');
-pudlTest('SELECT COLUMN_GET(`table`.`column`, `dynamic` AS BINARY(10)) FROM `table`');
+pudlTest("SELECT COLUMN_GET(`table`.`column`, 'dynamic' AS BINARY(10)) FROM `table`");
 
 
 
@@ -56,4 +56,4 @@ $db->string()->select(
 	),
 	'table'
 );
-pudlTest('SELECT COLUMN_GET(COLUMN_GET(`column`, `name` AS BINARY), `dynamic` AS INTEGER) FROM `table`');
+pudlTest("SELECT COLUMN_GET(COLUMN_GET(`column`, 'name' AS BINARY), 'dynamic' AS INTEGER) FROM `table`");
