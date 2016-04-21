077ad328a2968ce53d960bb42a45abcc38664108
Vince <git@darkain.com>
2016-04-21 12:51:48 -0700

    CLI test now checks to ensure main PHP files are parsable

--- test/cli.php
+++ test/cli.php
@@ -1,7 +1,21 @@
 <?php
 
 chdir(__DIR__);
+
+//REQUIRE ALL VERSIONS EVEN THOUGH WE ONLY USE SQL
+//THIS ENSURES THEY CAN AT LEAST BE PARSED BY PHP/HHVM!
+
+require_once('../pudlGalera.php');
+require_once('../pudlMsSql.php');
+require_once('../pudlMySql.php');
+require_once('../pudlMySqli.php');
+require_once('../pudlOdbc.php');
+require_once('../pudlPgSql.php');
+require_once('../pudlShell.php');
 require_once('../pudlSqlite.php');
+require_once('../pudlWeb.php');
+
+
 
 $db = new pudlSqlite([
 	'database'		=> 'test.db',
