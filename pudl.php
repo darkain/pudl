6a672034792c493149adfdbc8c0817b1ef7df8f1
Vince <github@darkain.com>
2016-10-26 09:41:43 -0700

    Version bump

--- pudl.php
+++ pudl.php
@@ -536,6 +536,6 @@ abstract class pudl {
 	private			$time			= 0;
 	private			$microtime		= 0.0;
 	protected		$string			= [];
-	public static	$version		= 'PUDL 2.4.3';
+	public static	$version		= 'PUDL 2.5.0';
 
 }
