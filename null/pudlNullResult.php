e7e1f88797bbaed930cb9ddb10e4872657bd09d9
Vince <git@darkain.com>
2017-03-23 14:58:16 -0700

    Fixing function definition

--- null/pudlNullResult.php
+++ null/pudlNullResult.php
@@ -9,14 +9,14 @@ class pudlNullResult extends pudlResult {
 
 
 
-	public function free()								{}
-	public function cell($row=0, $column=0)				{ return false; }
-	public function count()								{ return 0; }
-	public function fields()							{ return false; }
-	public function getField($column)					{ return false; }
-	public function seek($row)							{}
-	public function row($type=PUDL_ARRAY, $trim=true)	{ return false; }
-	public function error()								{ return 0; }
-	public function errormsg()							{ return ''; }
+	public function free()						{}
+	public function cell($row=0, $column=0)		{ return false; }
+	public function count()						{ return 0; }
+	public function fields()					{ return false; }
+	public function getField($column)			{ return false; }
+	public function seek($row)					{}
+	public function row($type=PUDL_ARRAY)		{ return false; }
+	public function error()						{ return 0; }
+	public function errormsg()					{ return ''; }
 
 }
