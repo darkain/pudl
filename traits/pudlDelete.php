264689620366de232a619927d5dcd46f3bb722de
Vince <git@darkain.com>
2017-06-09 11:15:23 -0700

    Added $db->deleteRow() - deletes a single row with LIMIT 1 Cleaned up $db->delete() Added unit tests for delete operations

--- traits/pudlDelete.php
+++ traits/pudlDelete.php
@@ -5,17 +5,25 @@ trait pudlDelete {
 
 
 	public function delete($table, $clause, $limit=false, $offset=false) {
-		$query  = 'DELETE ';
-		$query .= 'FROM ';
-		$query .= $this->_table($table);
-		$query .= $this->_clause($clause);
-		$query .= $this->_limit($limit, $offset);
-		return $this($query);
+		return $this(
+			'DELETE FROM ' .
+			$this->_table($table) .
+			$this->_clause($clause) .
+			$this->_limit($limit, $offset)
+		);
 	}
+
 
 
 	public function deleteId($table, $column, $id=false, $limit=false, $offset=false) {
 		return $this->delete($table, $this->_clauseId($column,$id), $limit, $offset);
 	}
+
+
+
+	public function deleteRow($table, $clause) {
+		return $this->delete($table, $clause, 1);
+	}
+
 
 }
