ccc27e975276a6a96f9e26049e87d91cc0f7a09d
Vince <git@darkain.com>
2017-09-14 10:39:47 -0700

    Added jsonInsert and jsonSet

--- test/json.php
+++ test/json.php
@@ -41,3 +41,95 @@ $db->string()->update('table', [
 ], true);
 
 pudlTest("UPDATE `table` SET `column_1`=JSON_REPLACE(`column_1`, '$.param_1', 'value_x'), `column_2`=JSON_REPLACE(`column_2`, '$.param_2', 'value_y') WHERE (1)");
+
+
+
+
+
+$db->string()->update('table', [
+	pudl::jsonSet('column', 'parameter', 'value'),
+], true);
+
+pudlTest("UPDATE `table` SET `column`=JSON_SET(`column`, '$.parameter', 'value') WHERE (1)");
+
+
+
+
+
+$db->string()->update('table', [
+	'x' => 1,
+	pudl::jsonSet('column', 'parameter', 'value'),
+], true);
+
+pudlTest("UPDATE `table` SET `x`=1, `column`=JSON_SET(`column`, '$.parameter', 'value') WHERE (1)");
+
+
+
+
+
+
+$db->string()->update('table', [
+	'x' => 1,
+	pudl::jsonSet('column', 'parameter', 'value'),
+	'y' => 2,
+], true);
+
+pudlTest("UPDATE `table` SET `x`=1, `column`=JSON_SET(`column`, '$.parameter', 'value'), `y`=2 WHERE (1)");
+
+
+
+
+
+
+$db->string()->update('table', [
+	pudl::jsonSet('column_1', 'param_1', 'value_x'),
+	pudl::jsonSet('column_2', 'param_2', 'value_y'),
+], true);
+
+pudlTest("UPDATE `table` SET `column_1`=JSON_SET(`column_1`, '$.param_1', 'value_x'), `column_2`=JSON_SET(`column_2`, '$.param_2', 'value_y') WHERE (1)");
+
+
+
+
+
+$db->string()->update('table', [
+	pudl::jsonInsert('column', 'parameter', 'value'),
+], true);
+
+pudlTest("UPDATE `table` SET `column`=JSON_INSERT(`column`, '$.parameter', 'value') WHERE (1)");
+
+
+
+
+
+$db->string()->update('table', [
+	'x' => 1,
+	pudl::jsonInsert('column', 'parameter', 'value'),
+], true);
+
+pudlTest("UPDATE `table` SET `x`=1, `column`=JSON_INSERT(`column`, '$.parameter', 'value') WHERE (1)");
+
+
+
+
+
+
+$db->string()->update('table', [
+	'x' => 1,
+	pudl::jsonInsert('column', 'parameter', 'value'),
+	'y' => 2,
+], true);
+
+pudlTest("UPDATE `table` SET `x`=1, `column`=JSON_INSERT(`column`, '$.parameter', 'value'), `y`=2 WHERE (1)");
+
+
+
+
+
+
+$db->string()->update('table', [
+	pudl::jsonInsert('column_1', 'param_1', 'value_x'),
+	pudl::jsonInsert('column_2', 'param_2', 'value_y'),
+], true);
+
+pudlTest("UPDATE `table` SET `column_1`=JSON_INSERT(`column_1`, '$.param_1', 'value_x'), `column_2`=JSON_INSERT(`column_2`, '$.param_2', 'value_y') WHERE (1)");
--- traits/pudlJson.php
+++ traits/pudlJson.php
@@ -10,6 +10,30 @@
 trait pudlJson {
 
 
+	static function jsonSet($column, $field, $value) {
+		return pudl::column(
+			$column,
+			pudl::json_set(
+				pudl::column($column),
+				'$.' . $field,
+				$value
+			)
+		);
+	}
+
+
+	static function jsonInsert($column, $field, $value) {
+		return pudl::column(
+			$column,
+			pudl::json_insert(
+				pudl::column($column),
+				'$.' . $field,
+				$value
+			)
+		);
+	}
+
+
 	static function jsonReplace($column, $field, $value) {
 		return pudl::column(
 			$column,
