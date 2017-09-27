310838d00105c58336aa978168622b177eb4bd04
Vince <git@darkain.com>
2017-09-27 15:39:58 -0700

    Fixing case

--- traits/pudlJson.php
+++ traits/pudlJson.php
@@ -93,7 +93,7 @@ trait pudlJson {
 				static::trim(static::column($column)),
 				''
 			),
-			PUDL::RAW("'{\"\":0}'")
+			pudl::raw("'{\"\":0}'")
 			//TODO: this SHOULD be an empty JSON object
 			//		MariaDB 10.2.8 and earlier won't accept this
 			//		We'll switch it back to '{}' when MariaDB is fixed
