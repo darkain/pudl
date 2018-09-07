858b994978f9dd6c108b52e4b4e333fbfcab3686
Vincent Milum Jr <git@darkain.com>
2018-09-06 20:31:45 -0700

    Adding pudl_require_extension

--- pudlInterfaces.php
+++ pudlInterfaces.php
@@ -7,6 +7,13 @@ function pudl_array($item) {
 
 
 
+function pudl_require_extension($extension) {
+	if (extension_loaded($extension)) return;
+	throw new pudlException('Required PHP extension is missing: ' . $extension);
+}
+
+
+
 class pudlException extends Exception {
 	public function __construct($message=null, $code=0, $previous=null) {
 		parent::__construct($message, $code, $previous);
