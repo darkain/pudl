b8b31931afcd8b120f2c5a1621c3bb3842336fb2
Vince <git@darkain.com>
2018-09-26 09:42:02 -0700

    Updating unit tests and notes

--- test/cell.inc.php
+++ test/cell.inc.php
@@ -18,13 +18,21 @@ pudlTest('SELECT `column` FROM `table` WHERE (`id`=`col`) LIMIT 1');
 
 //SELECT statement shortcut to get a single cell value using a clause
 //Returns string of the cell's value (false if not found)
+$db->string()->cell('table', 'column', ['id=col']);
+pudlTest('SELECT `column` FROM `table` WHERE (`id`=`col`) LIMIT 1');
+
+
+
+
+//SELECT statement shortcut to get a single cell value using a clause
+//Returns string of the cell's value (false if not found)
 $db->string()->cell('table', 'column', ['id'=>'value']);
 pudlTest("SELECT `column` FROM `table` WHERE (`id`='value') LIMIT 1");
 
 
 
 
-//SELECT statement shortcut to get a single cell value by another column's value
+//SELECT statement shortcut to get a single cell value using an ID
 //Returns string of the cell's value (false if not found)
 $db->string()->cellId('table', 'column', 'id', 'value');
 pudlTest("SELECT `column` FROM `table` WHERE (`id`='value') LIMIT 1");
@@ -32,10 +40,18 @@ pudlTest("SELECT `column` FROM `table` WHERE (`id`='value') LIMIT 1");
 
 
 
-//SELECT statement shortcut to get a single cell value by another column's value
+//SELECT statement shortcut to get a single cell value using an ID
 //Returns string of the cell's value (false if not found)
 $db->string()->cellId('table', 'column', 'id', pudl::unhex('abcdef1230'));
 pudlTest("SELECT `column` FROM `table` WHERE (`id`=UNHEX('abcdef1230')) LIMIT 1");
+
+
+
+
+//SELECT statement shortcut to get a single cell value using an ID
+//Returns string of the cell's value (false if not found)
+$db->string()->cellId('table', 'column', 'id', hex2bin('abcdef1230'));
+pudlTest("SELECT `column` FROM `table` WHERE (`id`=0xabcdef1230) LIMIT 1");
 
 
 
