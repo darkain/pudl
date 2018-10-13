a3dd34a6035e9934c93a83bd6275566f80ea9641
Vincent Milum Jr <git@darkain.com>
2018-10-13 00:26:49 -0700

    Cleaning up JSON shim

--- sql/pudlShellResult.php
+++ sql/pudlShellResult.php
@@ -145,13 +145,25 @@ if (!function_exists('json_last_error_msg')) {
 	/** @suppress PhanRedefineFunctionInternal */
 	function json_last_error_msg() {
 		switch (json_last_error()) {
-			case JSON_ERROR_DEPTH: $error = 'Maximum stack depth exceeded'; break;
-			case JSON_ERROR_STATE_MISMATCH: $error = 'Underflow or the modes mismatch'; break;
-			case JSON_ERROR_CTRL_CHAR: $error = 'Unexpected control character found'; break;
-			case JSON_ERROR_SYNTAX: $error = 'Syntax error, malformed JSON'; break;
-			case JSON_ERROR_UTF8: $error = 'Malformed UTF-8 characters, possibly incorrectly encoded';break;
-			default: return;
+			case JSON_ERROR_DEPTH:
+				throw new Exception('Maximum stack depth exceeded');
+			break;
+
+			case JSON_ERROR_STATE_MISMATCH:
+				throw new Exception('Underflow or the modes mismatch');
+			break;
+
+			case JSON_ERROR_CTRL_CHAR:
+				throw new Exception('Unexpected control character found');
+			break;
+
+			case JSON_ERROR_SYNTAX:
+				throw new Exception('Syntax error, malformed JSON');
+			break;
+
+			case JSON_ERROR_UTF8:
+				throw new Exception('Malformed UTF-8 characters, possibly incorrectly encoded');
+			break;
 		}
-		throw new Exception($error);
 	}
 }
