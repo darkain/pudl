8dd84757ed6b619e486beececd5c820dcb7b54a9
Vince <git@darkain.com>
2018-11-09 11:13:40 -0800

    Removing broken unit test

--- test/basic.inc.php
+++ test/basic.inc.php
@@ -1,11 +1,5 @@
 <?php
 
-//RAW unmodified SQL by invoking $db('SQL STATEMENT')
-$db->string()('SELECT * FROM table');
-pudlTest($db, 'SELECT * FROM table');
-
-
-
 
 //RAW unmodified SQL by invoking $db('SQL STATEMENT')
 $db->string();
